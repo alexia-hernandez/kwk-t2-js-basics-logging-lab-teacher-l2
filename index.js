b935f10398d99e834f939a14d53abfa08eae85dd
Alexia Hernandez <alexia.hernandez.17@tfacorps.org>
2018-06-28 14:45:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const driver = 'bob';
+
+console.log('this code was called'); 
+
+console.log('sets a variable driver equal to bob'); 
+
+console.log(driver);
+
+console.log(driver, 'is the driver variable name');
+
+     
+      
+  
+ 
+
