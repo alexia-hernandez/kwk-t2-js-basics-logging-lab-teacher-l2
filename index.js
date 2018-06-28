2a7a7acd351b82ea9cf6f9b08b6828307bc42887
Alexia Hernandez <alexia.hernandez.17@tfacorps.org>
2018-06-28 14:35:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const driver = 'bob';
+
+console.log('this code was called'); 
+
+const calledWithExactly = (console.log('sets a variable driver equal to bob')); 
+      expect(driver).to.equal('false');
+    if 
+      
+  
+ 
+
